4c07feac909b212566880af7bb3d557de142aca0
twendykirn <xan50rus@gmail.com>
2023-08-11 03:41:01 +0200

    refactor: update scale function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, PixelRatio, Platform } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+
+const ONE_UNIT = 4;
 
 //Default guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 350;
@@ -6,8 +9,25 @@ const guidelineBaseWidth = 350;
 const { width, height } = Dimensions.get('window');
 const [shortDimension] = width < height ? [width] : [height];
 
-export const scale = (size: number) => (shortDimension / guidelineBaseWidth) * size;
+const scale = (size: number) => (shortDimension / guidelineBaseWidth) * size;
 
-const ONE_UNIT = 4;
+const moderateScale = (size: number, factor = 0.5) => size + (scale(size) - size) * factor;
 
-export const getUnitValue = (multiplier: number) => scale(ONE_UNIT * multiplier);
+const defaultScale = (size: number) => {
+    let pixelRatio = PixelRatio.get();
+
+    const isAndroid = Platform.OS === 'android';
+
+    if (isAndroid && pixelRatio === 3 && height * PixelRatio.get() < (isAndroid ? 2000 : 1750)) {
+        pixelRatio = 2.7;
+        return Math.floor(PixelRatio.roundToNearestPixel((pixelRatio * size) / 3));
+    }
+
+    if (DeviceInfo.isTablet()) {
+        return Math.floor(moderateScale(size, 0.1));
+    }
+
+    return Math.floor(moderateScale(size));
+};
+
+export const getUnitValue = (multiplier: number) => defaultScale(ONE_UNIT * multiplier);
